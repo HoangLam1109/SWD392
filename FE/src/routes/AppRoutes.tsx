2e2cad0a80af093633756af37e4e5cdd3151de38
Phạm Hoàng Phát <phatphamhoang2016@gmail.com>
2026-01-30 02:17:22 +0700

    update login page

--- FE/src/routes/AppRoutes.tsx
+++ FE/src/routes/AppRoutes.tsx
@@ -1,10 +1,20 @@
-import HomePage from "@/pages/home-page"
+import HomePage from "@/pages/HomePage"
 import { Routes,Route } from "react-router-dom"
+import Login from "@/auth/page/Login"
+import Temp from "@/auth/page/Temp"
+import RegisterPage from "@/auth/page/Register"
+import ForgotPasswordPage from "@/auth/page/ForgotPasswordPage"
+import Dasboard from "@/pages/user/Dashboard"
 
 const AppRoutes = () => {
     return (
         <Routes>
             <Route path="/" element={<HomePage />} />
+            <Route path="login" element={<Login />} />
+            <Route path="temp" element={<Temp />} />
+            <Route path="register" element={<RegisterPage />} />
+            <Route path="forgot-password" element={<ForgotPasswordPage />} />
+            <Route path="dashboard" element={<Dasboard />} />
         </Routes>
     )
 }
