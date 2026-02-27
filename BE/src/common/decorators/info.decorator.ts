5dab219b8e26f0f8f54c4a3a7d8fedeb29a76572
VTL-Repo <vuthienloct@gmail.com>
2026-02-27 10:46:27 +0700

    Add some light changes to a few things

--- BE/src/common/decorators/info.decorator.ts
+++ BE/src/common/decorators/info.decorator.ts
@@ -1,8 +1,8 @@
-import { UserDocument } from '../entities/user.entity';
+import { IUser } from '../interfaces/user.interface';
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 
 export const GetUser = createParamDecorator((_, ctx: ExecutionContext) => {
   const req = ctx.switchToHttp().getRequest();
   const { userId, ...rest } = req.user;
-  return { _id: userId, ...rest } as Partial<UserDocument>;
+  return { _id: userId, ...rest } as Partial<IUser>;
 });
